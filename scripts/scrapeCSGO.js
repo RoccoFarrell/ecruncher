57ead2a74e11bf5428d57d38399f81d894a52df1
jmartin819 <josh.martin819@gmail.com>
2022-10-04 17:59:36 -0400

    Small modifications to CSGO script.

--- scripts/scrapeCSGO.js
+++ scripts/scrapeCSGO.js
@@ -26,30 +26,6 @@ function playerInfo () {
                 return resultsObj;
             })
 
-            //OLD MARTIN WAY
-            // let spans = await page.evaluate(() => {
-            //     let results = []
-            //     let headshot_percent = document.querySelectorAll('.stats-row')
-            //     let temp = Array.prototype.filter.call(headshot_percent, function(element){
-            //         return RegExp('Headshot').test(element.textContent)
-            //       })
-            //       temp.forEach((item) => {
-            //         results.push({
-            //             text: item.innerText
-            //         })
-            //     })
-
-            //     let player_name = document.querySelectorAll('.summaryNickname')
-            //       player_name.forEach((item) => {
-            //         results.push({
-            //             text: item.innerText
-            //         })
-            //     })
-
-            //     return results
-
-            // })
-
             browser.close()
             return resolve(playerInfoObj)
         } catch (e) {
@@ -63,7 +39,7 @@ function getStatsTable () {
         try {
             const browser = await puppeteer.launch()
             const page = await browser.newPage()
-            await page.goto("https://www.hltv.org/stats/players/matches/13239/qikert?startDate=2022-06-12&endDate=2022-09-12")
+            await page.goto("https://www.hltv.org/stats/players/matches/13239/qikert?startDate=2022-08-13&endDate=2022-09-13")
 
             let matchHistoryObj = await page.evaluate(() => {
                 let matchHistory = []
@@ -71,34 +47,22 @@ function getStatsTable () {
                 tableRows.forEach(row => {
                     let rowObj = {};
                     let rowTDs = row.querySelectorAll('td')
-                    rowObj.gameDate = rowTDs[0].innerText;
-                    rowObj.playerTeam = rowTDs[1].innerText;
-                    rowObj.opponentTeam = rowTDs[2].innerText;
-                    rowObj.map = rowTDs[3].innerText;
-                    rowObj.kd = rowTDs[4].innerText;
-                    rowObj.plusMinus = rowTDs[5].innerText;
-                    rowObj.rating = rowTDs[6].innerText;
-                    matchHistory.push(rowObj);
+                    rowObj.gameDate = rowTDs[0].innerText
+                    rowObj.playerTeam = rowTDs[1].innerText.split(" ")[0]
+                    rowObj.roundWins = rowTDs[1].innerText.split(" ")[1].replace("(","").replace(")","")
+                    rowObj.opponentTeam = rowTDs[2].innerText.split(" ")[0]
+                    rowObj.roundLosses = rowTDs[2].innerText.split(" ")[1].replace("(","").replace(")","")
+                    rowObj.map = rowTDs[3].innerText
+                    rowObj.kills = rowTDs[4].innerText.split(" - ")[0]
+                    rowObj.deaths = rowTDs[4].innerText.split(" - ")[1]
+                    rowObj.plusMinus = rowTDs[5].innerText
+                    rowObj.rating = rowTDs[6].innerText
+                    matchHistory.push(rowObj)
                 })
 
                 return matchHistory
 
             }) 
-
-            //OLD MARTIN DUMPSTER WAY
-            // let spans = await page.evaluate(() => {
-            //     let results = []
-            //     let table = document.querySelectorAll('.stats-table')
-            //     table.forEach((item) => {
-            //         results.push({
-            //             text: item.innerText
-            //         })
-            //     })
-
-            //     return results
-
-            // })
-
 
             browser.close()
             return resolve(matchHistoryObj)
