e50f4cddf8c9cbb5e5d4648caf35b3949a3d3a11
Josh Martin <josh.martin819@gmail.com>
2022-10-09 16:42:14 -0400

    Updated league script to pull all stats for a single players matchlist.

--- scripts/league.js
+++ scripts/league.js
@@ -9,32 +9,42 @@ function matchInfo () {
         try {
             const browser = await puppeteer.launch({ headless: false })
             const [page] = await browser.pages();
-            await page.goto("https://gol.gg/game/stats/44337/page-game/")
-            //await page.waitForNavigation();
+            await page.goto("https://gol.gg/players/player-matchlist/1932/season-S12/split-Summer/tournament-ALL/")
 
             page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
             let matchInfoObj = await page.evaluate(() => {
                 let resultsObj = {};
-                let selectorData = document.querySelectorAll(".playersInfosLine > tbody > tr")
+                let selectorData = document.querySelectorAll(".table_list > tbody > tr")
                 console.log(selectorData)
+
+                let playerName = document.querySelectorAll('h1:nth-of-type(1)')[0].innerText
 
                 let playerDataArr = []
                 if(selectorData.length > 0){
-                    selectorData.forEach(player => {
-                        let name = player.querySelectorAll('a.link-blanc')[0].innerText
-                        let playerKDA = player.querySelectorAll('td:nth-of-type(3)')[0].innerText.split('/')
+                    selectorData.forEach(match => {
+                        let champion = match.querySelectorAll('td:nth-of-type(1)')[0].innerText
+                        let result = match.querySelectorAll('td:nth-of-type(2)')[0].innerText
+                        let playerKDA = match.querySelectorAll('td:nth-of-type(3)')[0].innerText.split('/')
                         let playerKills = playerKDA[0]
                         let playerDeaths = playerKDA[1]
                         let playerAssists = playerKDA[2]
-                        let playerCS = player.querySelectorAll('td:nth-of-type(4)')[0].innerText
+                        let matchDuration = match.querySelectorAll('td:nth-of-type(5)')[0].innerText
+                        let matchDate = match.querySelectorAll('td:nth-of-type(6)')[0].innerText
+                        let game = match.querySelectorAll('td:nth-of-type(7)')[0].innerText
+                        let tournament = match.querySelectorAll('td:nth-of-type(8)')[0].innerText
 
                         playerDataArr.push({
-                            name: name,
+                            player: playerName,
+                            champion: champion,
+                            result: result,
                             kills: playerKills,
                             deaths: playerDeaths,
                             assists: playerAssists,
-                            cs: playerCS
+                            matchDuration: matchDuration,
+                            matchDate: matchDate,
+                            game: game,
+                            tournament: tournament
                         })
                     })
                 }
